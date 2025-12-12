6db7d3be2b84e0c80359d45f9fab7b5ebf475ef5
unknown <dzimadoeddison17@gmail.com>
2025-12-12 05:35:36 +0000

    new

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ session_start();
 define('DB_SERVER','localhost');
 define('DB_USER','root');
 define('DB_PASS', '');
-define('DB_NAME', 'mtn_developer');
+define('DB_NAME', 'test_developer');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
